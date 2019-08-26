43190ddd65b335ecfbb45797dc8677fffcf64ac3
Kamil Gumienny <kmgumienny@gmail.com>
2019-08-26 13:15:59 -0400

    Implemented basic functionality

--- proj1/proj1.c
+++ proj1/proj1.c
@@ -3,35 +3,51 @@ using namespace std;
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-extern char **environ; /* environment info */
 
+#define MAX_CHAR 128
+#define MAX_ARG 32
 
-
-
-
-
-main(int argc, char **argv)
-/* argc -- number of arguments */
-/* argv -- an array of strings */
-{
-    char *argvNew[5];
+void run_command(char **argv){
     int pid;
     if ((pid = fork()) < 0) {
         cerr << "Fork error\n";
         exit(1);
     }
     else if (pid == 0) {
-/* child process */
-        argvNew[0] = "/bin/ls";
-        argvNew[1] = "-l";
-        argvNew[2] = NULL;
-        if (execve(argvNew[0], argvNew, environ) < 0) {
-            cerr << "Execve error\n";
-            exit(1);
-        }
+		/* child process */
+		if (execvp(argv[0], &argv[0]) < 0) {
+			cerr << "Execve error\n";
+			exit(1);
+		}
+	}else {
+		/* parent */
+		wait(0); /* wait for the child to finish */
+	}
+}
+
+/*
+ * Main will want to do is check if the user wants to run a command
+ * in shell mode or simply run a command that would run as
+ * a background command.
+ */
+main(int argc, char **argv){
+/* argc -- number of arguments */
+/* argv -- an array of strings */
+
+    char prompt[] = "==>";
+
+    /*
+     * The user started the program with a command so the program
+     * will simply run that command
+    */
+    if(argc > 1){
+    	run_command(&argv[1]);
     }
-    else {
-/* parent */
-        wait(0); /* wait for the child to finish */
-    }
+
+    cout << prompt;
+//    while(0){
+//    	cout << prompt
+//		c
+//    }
+
 }
