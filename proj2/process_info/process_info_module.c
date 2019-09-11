4028e26d9fd8639fc3659b46add17d5b3e2abbec
Kamil Gumienny <kmgumienny@gmail.com>
2019-09-11 18:57:06 -0400

    Cleaned up code

--- proj2/process_info/process_info_module.c
+++ proj2/process_info/process_info_module.c
@@ -37,23 +37,19 @@ asmlinkage long new_sys_cs3013_syscall2(struct processinfo *info) {
     kernel_info.cutime = -1;
     kernel_info.cstime = -1;
 
-
-
     /*
      * http://tuxthink.blogspot.com/2011/03/listforeach-and-listentry.html
      * Article above explains how to iterate the list_head structure
      * of current_task's children and siblings
      */
 
-
     list_for_each(list, &current_task->children){
         child_task = list_entry(list, struct task_struct, children);
 
         this_child_pid = child_task->pid;
-        
-	if (kernel_info.cutime == -1 || (this_child_pid > kernel_info.youngest_child)) {
-	    printk(KERN_INFO "PID of child %d \n", child_task->pid);            
-	    kernel_info.youngest_child = child_task->pid;
+        if (kernel_info.cutime == -1 || (this_child_pid > kernel_info.youngest_child)) {
+          printk(KERN_INFO "PID of child %d \n", child_task->pid);
+          kernel_info.youngest_child = child_task->pid;
         }
 
         /* Make sure we don't have the -1 value if we do find children
@@ -69,8 +65,6 @@ asmlinkage long new_sys_cs3013_syscall2(struct processinfo *info) {
         kernel_info.cstime += cputime_to_usecs(child_task->stime);
     }
 
-
-
     /*
      * We iterate the second list_head containing this processes siblings
      */
@@ -79,32 +73,30 @@ asmlinkage long new_sys_cs3013_syscall2(struct processinfo *info) {
         sibling_task = list_entry(list, struct task_struct, children);
 
         this_sibling_pid = sibling_task->pid;
-	if (this_sibling_pid > 0){
-    	printk(KERN_INFO "PID of sibling %d \n", this_sibling_pid);  
-        
-	if (this_sibling_pid < kernel_info.pid && (kernel_info.younger_sibling == -1 ||this_sibling_pid > kernel_info.younger_sibling)){
+        if (this_sibling_pid > 0){
+          printk(KERN_INFO "PID of sibling %d \n", this_sibling_pid);
+
+          if (this_sibling_pid < kernel_info.pid && (kernel_info.younger_sibling == -1 ||this_sibling_pid > kernel_info.younger_sibling)){
             kernel_info.younger_sibling = this_sibling_pid;
-        }
-	else if (this_sibling_pid > kernel_info.pid && (kernel_info.older_sibling == -1 ||this_sibling_pid < kernel_info.older_sibling)) {
+          }
+          else if (this_sibling_pid > kernel_info.pid && (kernel_info.older_sibling == -1 ||this_sibling_pid < kernel_info.older_sibling)) {
             kernel_info.older_sibling = this_sibling_pid;
+          }
         }
-}
-    }
+      }
 
-
-    if (copy_to_user(info, &kernel_info, sizeof kernel_info)) {
+      if (copy_to_user(info, &kernel_info, sizeof kernel_info)) {
         printk(KERN_INFO "Copying to user failed \n");
         return EFAULT;
     }
-
-
     return 0;
 }
+
 
 static unsigned long **find_sys_call_table(void) {
   unsigned long int offset = PAGE_OFFSET;
   unsigned long **sct;
-  
+
   while (offset < ULLONG_MAX) {
     sct = (unsigned long **)offset;
 
@@ -113,10 +105,10 @@ static unsigned long **find_sys_call_table(void) {
                 (unsigned long) sct);
       return sct;
     }
-    
+
     offset += sizeof(void *);
   }
-  
+
   return NULL;
 }
 
@@ -140,7 +132,7 @@ static void disable_page_protection(void) {
 
 static void enable_page_protection(void) {
   /*
-   See the above description for cr0. Here, we use an OR to set the 
+   See the above description for cr0. Here, we use an OR to set the
    16th bit to re-enable write protection on the CPU.
   */
   write_cr0 (read_cr0 () | 0x10000);
@@ -149,11 +141,11 @@ static void enable_page_protection(void) {
 static int __init interceptor_start(void) {
   /* Find the system call table */
   if(!(sys_call_table = find_sys_call_table())) {
-    /* Well, that didn't work. 
+    /* Well, that didn't work.
        Cancel the module loading step. */
     return -1;
   }
-  
+
   /* Store a copy of all the existing functions */
   ref_sys_cs3013_syscall2 = (void *)sys_call_table[__NR_cs3013_syscall2];
 
@@ -161,9 +153,9 @@ static int __init interceptor_start(void) {
   disable_page_protection();
 
   sys_call_table[__NR_cs3013_syscall2] = (unsigned long *)new_sys_cs3013_syscall2;
-  
+
   enable_page_protection();
-  
+
   /* And indicate the load was successful */
   printk(KERN_INFO "Loaded interceptor!");
 
@@ -174,7 +166,7 @@ static void __exit interceptor_end(void) {
   /* If we don't know what the syscall table is, don't bother. */
   if(!sys_call_table)
     return;
-  
+
   /* Revert all system calls to what they were before we began. */
   disable_page_protection();
   sys_call_table[__NR_cs3013_syscall2] = (unsigned long *)ref_sys_cs3013_syscall2;
