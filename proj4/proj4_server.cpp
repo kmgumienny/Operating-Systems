66e98833118409c89cce34e96241c1b04b455bac
Student <kmgumienny@gmail.com>
2019-10-06 12:31:29 -0400

    starting threading

--- proj4/proj4_server.cpp
+++ proj4/proj4_server.cpp
@@ -14,8 +14,12 @@ using namespace std;
 #include <fstream>
 
 #define MAX_BUFFER 256
+#define MAX_THREADS 15
 
 sem_t* semaphores;
+int num_threads;
+char ** list_files;
+
 int bad_files;
 int directories;
 int regular_files;
@@ -23,6 +27,12 @@ int special_files;
 long int regular_file_size;
 int text_files;
 long int text_file_size;
+
+
+void* thread_proccess(void* argv) {
+    int index = (long) argv;
+    return 0;
+}
 
 
 int check_is_textfile(char* file_name){
@@ -46,7 +56,6 @@ int main(int argc, char *argv[]) {
     struct stat *statbuf;
     statbuf = (struct stat*)malloc(sizeof(struct stat));
     FILE *fp;
-//    char* file_buffer = (char*)malloc(sizeof(char) * MAX_BUFFER);
     char* file_buffer = (char*)malloc(sizeof(char) * MAX_BUFFER);
     size_t buffer_size = MAX_BUFFER;
 
@@ -105,6 +114,77 @@ int main(int argc, char *argv[]) {
         cout << "Text File Bytes: " << text_file_size << endl;
         fclose(fp);
         return 0;
-    }
+    }else
+        if (argc == 3 && !strcmp(argv[1], "thread")){
+            int thread_status, total_threads_ran = 0, this_thread_iteration = 0;
+            void* status;
 
+
+
+            num_threads = atoi(argv[2]);
+            if (num_threads > MAX_THREADS ){
+                cout << "Number of threads must be less than 15. Setting to 15" << endl;
+                num_threads = 15;
+            }
+            if (num_threads < 1){
+                cout << "Number of threads must be 1 or greater. Setting to 1" << endl;
+                num_threads = 1;
+            }
+            cout << "Running in thread mode with " << num_threads << " threads." << endl;
+
+            list_files = (char **)(malloc(num_threads * sizeof(char *)));
+            for (int i = 0; i < num_threads; i++){
+                list_files[i] = (char *)malloc(MAX_BUFFER * sizeof(char));
+            }
+
+            pthread_t threads[num_threads];
+            semaphores = (sem_t*)malloc(num_threads * sizeof(sem_t));
+            for (int i = 0; i < num_threads; i++){
+                sem_init(&semaphores[i], 1, 1);
+            }
+
+            while(getline(&file_buffer, &buffer_size, fp) != -1) {
+                // remove newline character and replace with null terminator for stat
+                file_buffer[strlen(file_buffer) - 1] = '\0';
+                this_thread_iteration = total_threads_ran % num_threads;
+
+                if(total_threads_ran > num_threads){
+                    if(pthread_join(threads[this_thread_iteration], NULL)){
+                        cout << "Error joining threads. Exiting." << endl;
+                        exit(1);
+                    }
+                }
+                strcpy(list_files[this_thread_iteration], file_buffer);
+                if(pthread_create(&threads[this_thread_iteration], NULL, thread_proccess, (void*)this_thread_iteration)){
+                    cout << "Error creating threads. Exiting." << endl;
+                    exit(1);
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+        }else{
+            cout << "Please refer to the readme on commandline arguments" << endl;
+            exit(1);
+        }
+
+    for (int i = 0; i < num_threads; i++){
+        free(list_files[i]);
+    }
+    free(list_files);
+    free(semaphores);
+
+
+    return 0;
 }
